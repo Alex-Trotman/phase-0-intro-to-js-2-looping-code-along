e8eaf85ce3276e782313e8f15b89e50810b10053
Alex <alextrotman04@gmail.com>
2023-07-19 08:03:35 -0500

    committing so I can finish this at work

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(array, event) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push( `Thank you, ${array[i]}, for the wonderful ${event} gift!` )
+    }
+    return newArray;
+}
+
+
+
+
+
+
+// console.log(writeCards(["Dave", "John", "Bob"], "Birthday"))
+
